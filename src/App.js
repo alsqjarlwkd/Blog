4952f72a8128a8fae4967280426ff4e37a47f194
MinBeomgi <alsqjarlwkd@naver.com>
2021-07-19 20:00:03 +0900

    라우터 수정
    라우터 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 import Navbar from './Component/Navbar'
 import Home from './Component/Home'
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import BlogCreate from './Component/BlogCreate';
+import BlogDetails from './Component/BlogDetails';
+import FixContent  from './Component/FixContent';
+import NotFound from './Component/NotFound';
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar></Navbar>
       <div className="content">
-        <Home></Home>
+        <Switch>
+        <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/create">
+            <BlogCreate></BlogCreate>
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails></BlogDetails>
+          </Route>
+          <Route path="/FixContent/:id">
+            <FixContent></FixContent>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
