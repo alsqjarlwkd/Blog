14d9f0de87be1b878b04eaebcb335380cf9c30a7
MinBeomgi <alsqjarlwkd@naver.com>
2021-07-20 19:18:50 +0900

    좋아요 버튼 클릭시 좋아요 카운트 업데이트 추가(PUT)메소드 사용
    좋아요 버튼 클릭시 좋아요 카운트 업데이트 추가(PUT)메소드 사용

--- src/Component/BlogDetails.js
+++ src/Component/BlogDetails.js
@@ -7,12 +7,27 @@ const BlogDetails = () => {
     const {id}=useParams();
     const {data:blogs,isLoading,error}=useFetch('http://localhost:3001/Blog/'+id)
     const History = useHistory();
-    
     const handleClick=()=>{
         fetch('http://localhost:3001/Blog/'+ blogs.id,{
             method:"DELETE"
         }).then(()=>{
             alert("Are you really Delete this Blog?")
+            History.push("/")
+        })
+    }
+    const ClickLikes=()=>{
+        fetch('http://localhost:3001/Blog/'+ blogs.id,{
+            method:"PUT",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({
+                "title": blogs.title,
+                "Body": blogs.Body,
+                "author": blogs.author,
+                "Likes": blogs.Likes+1,
+                "id": blogs.id
+            })
+        }).then(()=>{
+            alert("are you really Like this blog?")
             History.push("/")
         })
     }
@@ -25,6 +40,7 @@ const BlogDetails = () => {
                     <Link to="/"><button style={{marginBottom:"50px"}}>Back</button></Link>
                     <h2>{blogs.title}</h2>
                     <p>Written by {blogs.author}</p>
+                    <button style={{marginTop:"50px"}} onClick={ClickLikes}>Likes:{blogs.Likes}</button>
                     <div style={{border:"thick double #f1356d",padding:"3rem"}}>{blogs.Body}</div>
                     <button onClick={handleClick}>Delete</button>
                     <Link to={`/FixContent/`+id}><button style={{marginLeft:"50px"}}>Fix Content</button></Link>
